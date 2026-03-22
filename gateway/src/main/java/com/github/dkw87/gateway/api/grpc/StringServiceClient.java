54964506fb460cdf9cb0c5611f16342d5b64ac44
DKW87 <dannykwant@gmail.com>
2026-03-22 09:26:56 +0100

    feat(StringServiceClient): init

---
+++ gateway/src/main/java/com/github/dkw87/gateway/api/grpc/StringServiceClient.java
@@ -0,0 +1,23 @@
+package com.github.dkw87.gateway.api.grpc;
+
+import com.github.dkw87.grpc.proto.StringResponse;
+import com.github.dkw87.grpc.proto.StringServiceGrpc;
+import com.google.protobuf.Empty;
+import org.springframework.grpc.client.GrpcChannelFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringServiceClient {
+
+    // TODO: replace for non-blocking stub once initial project is up and running
+    private final StringServiceGrpc.StringServiceBlockingStub stub;
+
+    public StringServiceClient(GrpcChannelFactory channelFactory) {
+        this.stub = StringServiceGrpc.newBlockingStub(channelFactory.createChannel("service"));
+    }
+
+    public StringResponse getResponse() {
+        return stub.getString(Empty.getDefaultInstance());
+    }
+
+}
