40fca94dabe873ef6774c3b726b1dac7b34b2afb
DKW87 <dannykwant@gmail.com>
2026-03-24 20:19:44 +0100

    feat(client): AddressServiceClient init

---
+++ registration-service/src/main/java/com/github/dkw87/registrationservice/api/grpc/client/AddressServiceClient.java
@@ -0,0 +1,26 @@
+package com.github.dkw87.registrationservice.api.grpc.client;
+
+import com.github.dkw87.grpc.proto.address.AddressRequest;
+import com.github.dkw87.grpc.proto.address.AddressResponse;
+import com.github.dkw87.grpc.proto.address.AddressServiceGrpc;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.grpc.client.GrpcChannelFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AddressServiceClient {
+
+    private final AddressServiceGrpc.AddressServiceBlockingStub stub;
+
+    public AddressServiceClient(GrpcChannelFactory channelFactory) {
+        this.stub = AddressServiceGrpc.newBlockingStub(channelFactory.createChannel("address-service"));
+    }
+
+    public AddressResponse execute(long id) {
+        log.info("Requesting response from AddressServiceGrpc...");
+        AddressRequest request = AddressRequest.newBuilder().setId(id).build();
+        return stub.getAddress(request);
+    }
+
+}
