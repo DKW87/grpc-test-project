c0f5afe7dd39b05f45e446ff2694035e72accd44
DKW87 <dannykwant@gmail.com>
2026-03-24 20:24:42 +0100

    feat(client): PersonServiceClient init

---
+++ registration-service/src/main/java/com/github/dkw87/registrationservice/api/grpc/client/PersonServiceClient.java
@@ -0,0 +1,26 @@
+package com.github.dkw87.registrationservice.api.grpc.client;
+
+import com.github.dkw87.grpc.proto.person.PersonRequest;
+import com.github.dkw87.grpc.proto.person.PersonResponse;
+import com.github.dkw87.grpc.proto.person.PersonServiceGrpc;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.grpc.client.GrpcChannelFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class PersonServiceClient {
+
+    private final PersonServiceGrpc.PersonServiceBlockingStub stub;
+
+    public PersonServiceClient(GrpcChannelFactory channelFactory) {
+        this.stub = PersonServiceGrpc.newBlockingStub(channelFactory.createChannel("person-service"));
+    }
+
+    public PersonResponse execute(long id) {
+        log.info("Requesting response from PersonServiceGrpc for id {}...", id);
+        PersonRequest request = PersonRequest.newBuilder().setId(id).build();
+        return stub.getPerson(request);
+    }
+
+}
