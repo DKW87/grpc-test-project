1ad5123ea090c8dfbe723e77ea52ab76aafc2613
DKW87 <dannykwant@gmail.com>
2026-03-24 20:31:44 +0100

    feat(impl): RegistrationServiceImpl init

---
+++ registration-service/src/main/java/com/github/dkw87/registrationservice/api/grpc/impl/RegistrationServiceImpl.java
@@ -0,0 +1,42 @@
+package com.github.dkw87.registrationservice.api.grpc.impl;
+
+import com.github.dkw87.grpc.proto.address.Address;
+import com.github.dkw87.grpc.proto.person.Person;
+import com.github.dkw87.grpc.proto.registration.RegistrationRequest;
+import com.github.dkw87.grpc.proto.registration.RegistrationResponse;
+import com.github.dkw87.grpc.proto.registration.RegistrationServiceGrpc;
+import com.github.dkw87.registrationservice.api.grpc.client.AddressServiceClient;
+import com.github.dkw87.registrationservice.api.grpc.client.PersonServiceClient;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+@RequiredArgsConstructor
+@Slf4j
+public class RegistrationServiceImpl extends RegistrationServiceGrpc.RegistrationServiceImplBase {
+
+    private final PersonServiceClient psClient;
+    private final AddressServiceClient asClient;
+
+    @Override
+    public void getRegistration(RegistrationRequest request, StreamObserver<RegistrationResponse> responseObserver) {
+        log.info("Received request for getRegistration() for id {}...", request.getId());
+
+        Address address = asClient.execute(request.getId()).getAddress();
+        Person person =  psClient.execute(request.getId()).getPerson();
+
+        RegistrationResponse response = RegistrationResponse.newBuilder()
+                .setEventName("Wimbledon High Tea Party")
+                .setWantsToReceiveNewsletter(true)
+                .setAddress(address)
+                .setPerson(person)
+                .build();
+
+        responseObserver.onNext(response);
+        log.info("Successfully sent RegistrationResponse for id {}", request.getId());
+        responseObserver.onCompleted();
+    }
+
+}
