2a1b643dc9e4aa5e87dbd89ab41be7317ac6e97c
DKW87 <dannykwant@gmail.com>
2026-03-25 21:10:29 +0100

    feat(jsonformat): add protobuf-java-utils to print response in json format

--- gateway/src/main/java/com/github/dkw87/gateway/api/rest/RegistrationServiceController.java
+++ gateway/src/main/java/com/github/dkw87/gateway/api/rest/RegistrationServiceController.java
@@ -2,6 +2,8 @@ package com.github.dkw87.gateway.api.rest;
 
 import com.github.dkw87.gateway.api.grpc.RegistrationServiceClient;
 import com.github.dkw87.grpc.proto.registration.RegistrationResponse;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +23,14 @@ public class RegistrationServiceController {
     public String getId(@PathVariable("id") long id) {
         log.info("Executing RegistrationServiceClient for id {}...", id);
         RegistrationResponse response = rsclient.execute(id);
+        String registration = null;
 
-        String registration = response.toString();
+        try {
+            registration = JsonFormat.printer().alwaysPrintFieldsWithNoPresence().print(response);
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error while creating JSON response for {} \n", id, e);
+        }
+
         log.info("Response from RegistrationServiceClient: {}", registration);
         return registration;
     }
