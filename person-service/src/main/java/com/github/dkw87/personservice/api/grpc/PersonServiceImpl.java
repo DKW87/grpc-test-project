c9ba0897c76c9931cebb6361fc98e2b33bba2913
DKW87 <dannykwant@gmail.com>
2026-03-25 20:32:54 +0100

    feat(datafaker): implement faker in PersonServiceImpl

--- person-service/src/main/java/com/github/dkw87/personservice/api/grpc/PersonServiceImpl.java
+++ person-service/src/main/java/com/github/dkw87/personservice/api/grpc/PersonServiceImpl.java
@@ -3,9 +3,11 @@ package com.github.dkw87.personservice.api.grpc;
 import com.github.dkw87.grpc.proto.person.*;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
+import net.datafaker.Faker;
 import org.springframework.grpc.server.service.GrpcService;
 
 import java.util.List;
+import java.util.Locale;
 
 @GrpcService
 @Slf4j
@@ -14,14 +16,15 @@ public class PersonServiceImpl extends PersonServiceGrpc.PersonServiceImplBase {
     @Override
     public void getPerson(PersonRequest request,  StreamObserver<PersonResponse> responseObserver) {
         log.info("Received request for getPerson() for id {}...", request.getId());
+        Faker faker = new Faker(Locale.ENGLISH);
 
-        List<String> hobbys = List.of("Cricket", "Badminton", "Sipping tea");
+        List<String> hobbys = List.of(faker.hobby().activity(), faker.hobby().activity(), faker.hobby().activity());
 
         Person person = Person.newBuilder()
-                .setName("Gerald Richard Peter Callingwood")
-                .setGender("m")
-                .setDob("01-10-1947")
-                .setPob("Birmingham")
+                .setName(faker.name().fullName())
+                .setGender(faker.gender().binaryTypes())
+                .setDob(faker.timeAndDate().birthday("dd-MM-yyyy"))
+                .setPob(faker.address().city())
                 .addAllHobbys(hobbys) // dont use setter but addAll to add whole List<>, setter can overwrite specific index w/ value
                 .build();
 
