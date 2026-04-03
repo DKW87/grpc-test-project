4e6513b6287cb1f5239bb1bb913637e673f681cf
DKW87 <dannykwant@gmail.com>
2026-04-03 19:23:46 +0200

    refactor(RegistrationServiceImpl): now call two services at the same time using CompletableFuture

--- registration-service/src/main/java/com/github/dkw87/registrationservice/api/grpc/impl/RegistrationServiceImpl.java
+++ registration-service/src/main/java/com/github/dkw87/registrationservice/api/grpc/impl/RegistrationServiceImpl.java
@@ -1,12 +1,16 @@
 package com.github.dkw87.registrationservice.api.grpc.impl;
 
 import com.github.dkw87.grpc.proto.address.Address;
+import com.github.dkw87.grpc.proto.address.AddressResponse;
 import com.github.dkw87.grpc.proto.person.Person;
+import com.github.dkw87.grpc.proto.person.PersonResponse;
 import com.github.dkw87.grpc.proto.registration.RegistrationRequest;
 import com.github.dkw87.grpc.proto.registration.RegistrationResponse;
 import com.github.dkw87.grpc.proto.registration.RegistrationServiceGrpc;
 import com.github.dkw87.registrationservice.api.grpc.client.AddressServiceClient;
 import com.github.dkw87.registrationservice.api.grpc.client.PersonServiceClient;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +20,8 @@ import org.springframework.util.StringUtils;
 
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 @GrpcService
@@ -32,8 +38,22 @@ public class RegistrationServiceImpl extends RegistrationServiceGrpc.Registratio
     public void getRegistration(RegistrationRequest request, StreamObserver<RegistrationResponse> responseObserver) {
         log.info("Received request for getRegistration() for id {}...", request.getId());
 
-        final Address address = addressServiceClient.execute(request.getId()).getAddress();
-        final Person person = personServiceClient.execute(request.getId()).getPerson();
+        final CompletableFuture<AddressResponse> addressFuture =
+                CompletableFuture.supplyAsync(() -> addressServiceClient.execute(request.getId()));
+        final CompletableFuture<PersonResponse> personFuture =
+                CompletableFuture.supplyAsync(() -> personServiceClient.execute(request.getId()));
+
+        CompletableFuture.allOf(addressFuture, personFuture).join();
+
+        Address address = null;
+        Person person = null;
+
+        try {
+            address = addressFuture.get().getAddress();
+            person = personFuture.get().getPerson();
+        } catch (ExecutionException | InterruptedException e) {
+            responseObserver.onError(Status.INTERNAL.withCause(e).asRuntimeException());
+        }
 
         final String adjective = StringUtils.capitalize(FAKER.word().adjective());
         final String hobby = capitalizeEachWord(FAKER.hobby().activity());
