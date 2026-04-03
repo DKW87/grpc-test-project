515adb46b7af86a22a19dbf220e0f8b60856e50b
DKW87 <dannykwant@gmail.com>
2026-04-03 16:15:53 +0200

    feat(util): init GrpcUtil

---
+++ gateway/src/main/java/com/github/dkw87/gateway/api/rest/util/GrpcUtil.java
@@ -0,0 +1,31 @@
+package com.github.dkw87.gateway.api.rest.util;
+
+import io.grpc.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GrpcUtil {
+
+    private boolean isGrpcWarnLevel(Status.Code code) {
+        return switch (code) {
+            case INVALID_ARGUMENT, NOT_FOUND, ALREADY_EXISTS, PERMISSION_DENIED,
+                 UNAUTHENTICATED, CANCELLED, FAILED_PRECONDITION, OUT_OF_RANGE -> true;
+            default -> false;
+        };
+    }
+
+    private int mapGrpcToHttp(Status.Code code) {
+        return switch (code) {
+            case INVALID_ARGUMENT, FAILED_PRECONDITION, OUT_OF_RANGE -> 400;
+            case NOT_FOUND -> 404;
+            case ALREADY_EXISTS -> 409;
+            case UNAUTHENTICATED -> 401;
+            case PERMISSION_DENIED -> 403;
+            case RESOURCE_EXHAUSTED -> 429;
+            case UNIMPLEMENTED -> 501;
+            case UNAVAILABLE, DEADLINE_EXCEEDED -> 503;
+            default -> 500;
+        };
+    }
+
+}
