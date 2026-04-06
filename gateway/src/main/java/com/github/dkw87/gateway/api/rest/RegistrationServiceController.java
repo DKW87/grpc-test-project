6a6e91c572e1b6294707d98db11235a5a6cf51b5
DKW87 <dannykwant@gmail.com>
2026-04-06 18:53:30 +0200

    refactor(RegistrationServiceController): now properly return ResponseEntity<DTO>

--- gateway/src/main/java/com/github/dkw87/gateway/api/rest/RegistrationServiceController.java
+++ gateway/src/main/java/com/github/dkw87/gateway/api/rest/RegistrationServiceController.java
@@ -1,12 +1,14 @@
 package com.github.dkw87.gateway.api.rest;
 
 import com.github.dkw87.gateway.api.grpc.RegistrationServiceClient;
+import com.github.dkw87.gateway.api.rest.dto.RegistrationResponseDTO;
 import com.github.dkw87.grpc.proto.registration.RegistrationResponse;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,15 +27,15 @@ public class RegistrationServiceController {
     private final RegistrationServiceClient registrationServiceClient;
 
     @GetMapping("/{id}")
-    public String getId(@PathVariable @Positive long id) throws InvalidProtocolBufferException {
+    public ResponseEntity<RegistrationResponseDTO> getId(@PathVariable @Positive long id) throws InvalidProtocolBufferException {
         log.info("Executing RegistrationServiceClient for id {}...", id);
 
-        final RegistrationResponse response = registrationServiceClient.execute(id);
+        final RegistrationResponse serviceResponse = registrationServiceClient.execute(id);
 
-        final String registration = JSON_PRINTER.print(response);
+        final RegistrationResponseDTO clientResponse = RegistrationResponseDTO.from(serviceResponse);
 
-        log.info("Response from RegistrationServiceClient: {}", registration);
-        return registration;
+        log.info("Response from RegistrationServiceClient: {}", clientResponse);
+        return ResponseEntity.ok(clientResponse);
     }
 
 }
